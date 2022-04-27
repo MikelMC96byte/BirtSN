60d5c94e7e4b814f84b526d8cfe74a006ee13e52
Mikel Maldonado <71812437+MikelMC96byte@users.noreply.github.com>
2022-04-27 11:29:34 +0200

    prueba despliegue

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/BirtSN'));
+app.use(express.static(__dirname + '/dist/birtsn'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/BirtSN/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/birtsn/index.html'));
 });
 app.listen(process.env.PORT || 8080);
